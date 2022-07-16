f54e562f41736900cf07db7fca3e82a60a3c6c21
Luan Ciribelli <luanreisciribelli@gmail.com>
2022-07-16 12:12:35 -0300

    Resolvendo conflito

--- src/Algoritmos/NaoInformados/Backtracking.cpp
+++ src/Algoritmos/NaoInformados/Backtracking.cpp
@@ -14,54 +14,7 @@ using namespace std;
 
 class Backtracking {
 public:
-<<<<<<< HEAD
-    static void run(Estado *inicial, Estado *final)
-    {
-        short status = 0; // -1 = FRACASSO; 1 = SUCESSO; 0 = EM PROCESSO
-        Fila<Estado *> *abertos = new Fila<Estado *>();
-        Lista<Estado *> *fechados = new Lista<Estado *>();
-        Estado *atual;
-        Estado *aux;
-        abertos->inserir(inicial);
-        while (status == 0)
-        {
-            if (abertos->estaVazio())
-            {
-                cout << "FRACASSO" << endl;
-                status = -1;
-            }
-            else
-            {
-                atual = abertos->remover();
-                if (fechados->contem(atual))
-                {
-                    delete atual;
-                }
-                else
-                {
-                    atual->print();
-                    cout << "-->";
-                    if (atual->equals(final))
-                    {
-                        cout << "SUCESSO" << endl;
-                        status = 1;
-                    }
-                    else
-                    {
-                        for (int i = inicial->getNumMovimentos(); i >= 1; i--)
-                        {
-                            aux = atual->movimentar(i);
-                            if (aux != NULL && !fechados->contem(aux))
-                            {
-                                aux->print();
-                                abertos->inserir(aux);
-                            }
-                        }
-                        cout << endl;
-                        fechados->inserir(atual);
-                    }
-                }
-=======
+
   static void run(Estado *inicial, Estado *final) {
     short status = 0; // -1 = FRACASSO; 1 = SUCESSO; 0 = EM PROCESSO
     Pilha<Estado *> *abertos = new Pilha<Estado *>();
@@ -94,9 +47,7 @@ public:
               if (aux != NULL && !fechados->contem(aux)) {
                 aux->print();
                 abertos->inserir(aux);
-              }
->>>>>>> feature/backtracking
-            }
+              }            }
             cout << endl;
             fechados->inserir(atual);
           }
