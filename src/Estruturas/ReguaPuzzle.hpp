4e26e0da6af7405701d665240fc7778573dec91a
Luan Ciribelli <luanreisciribelli@gmail.com>
2022-07-26 12:39:32 -0300

    Arrumando RegPuz

--- src/Estruturas/ReguaPuzzle.hpp
+++ src/Estruturas/ReguaPuzzle.hpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 /*******************************************
@@ -14,40 +13,223 @@ using namespace std;
  *******************************************/
 class ReguaPuzzle : public Estado {
 
+public:
+  ReguaPuzzle() {
+    this->n = 0;
+    this->regua[0] = '\0';
+    this->numMov = 4;
+    calculaEstadoFinal();
+  };
+
+  ReguaPuzzle(int n, vector<char> regua) {
+    this->n = n;
+    this->regua = regua;
+    this->numMov = 4;
+    // calculaEstadoFinal();
+  };
+  ~ReguaPuzzle() {
+    this->regua.clear();
+    this->estadoFinal.clear();
+  };
+
+  // Setters
+  void setN(int novoN) { this->n = novoN; };
+  void setRegua(vector<char> regua) { this->regua = regua; };
+
+  // getters
+  int getN() { return this->n; };
+  vector<char> getRegua() { return this->regua; };
+  int getNumMovimentos() { return this->numMov; };
+  vector<char> getEstadoFinal() { return this->estadoFinal; };
+  int getCusto() { return -1; };
+  int getHeuristica() { return -1; };
+
+  bool equals(Estado *estado) {
+    cout << "Estado vazio: equals" << endl;
+    return false;
+  };
+
+  // Imprimir estado atual
+  void print() {
+
+    for (int i = 0; i <= n; i++) {
+      cout << this->regua[i] << " ";
+    }
+    cout << endl;
+  };
+
+  bool validade() {
+    if (this->n > 0) {
+      // Array que conta o numero de brancos, Pretos e espaços. Nessa ordem;
+      int conta[3] = {0, 0, 0};
+      for (int i = 0; i <= n; i++) {
+
+        if (this->regua[i] == 'B')
+          conta[0] += 1;
+
+        else if (this->regua[i] == 'P')
+          conta[1] += 1;
+
+        else 
+/*
+          cout << "DEBUG" << '\n';
+          cout << this->regua[i] << " ";
+*/      if (this->regua[i] == '-')
+          conta[2] += 1;
+        
+      }
+
+      if (conta[0] == (((n - 1) / 2)) && conta[1] == (((n - 1) / 2)) &&
+          conta[2] == 1) {
+        return true;
+      } else {
+
+        cout << "Peças brancas " << conta[0] << " Peças Pretas " << conta[1]
+             << " Espaço " << conta[2] << endl;
+        return false;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  // Funções de estado
+  Estado *movimentar(int indexMovimento, bool custo, Estado *estadoFinal) {
+
+    ReguaPuzzle *reguaNova = new ReguaPuzzle(this->n, this->regua);
+
+    switch (indexMovimento) {
+
+    case 1: {
+      // Bloco a esquerda move para o buraco, se tiver membro a esquerda
+      int pos = 0;
+
+      for (char &c : reguaNova->getRegua()) {
+        if (c == '-') {
+          break;
+        } else {
+          pos++;
+        }
+      }
+
+      if (pos <= reguaNova->getRegua().size()) {
+        if (pos - 1 >= 0) {
+          vector<char> aux = reguaNova->getRegua();
+          iter_swap(aux.begin() + pos - 1, aux.begin() + pos);
+          reguaNova->setRegua(aux);
+        }
+
+      } else {
+        reguaNova = NULL;
+      }
+      break;
+    }
+    case 2: {
+      // Bloco a Direita move para o buraco, se tiver membro a direita
+      int pos = 0;
+
+      for (char &c : reguaNova->getRegua()) {
+        if (c == '-') {
+          break;
+        } else {
+          pos++;
+        }
+      }
+
+      if (pos <= reguaNova->getRegua().size()) {
+        if (pos + 1 <= reguaNova->getRegua().size()) {
+          vector<char> aux = reguaNova->getRegua();
+          iter_swap(aux.begin() + pos + 1, aux.begin() + pos);
+          reguaNova->setRegua(aux);
+        }
+
+      } else {
+        reguaNova = NULL;
+      }
+      break;
+    }
+    case 3: {
+      // Bloco dois a esquerda move para o buraco, se tiver como
+      int pos = 0;
+
+      for (char &c : reguaNova->getRegua()) {
+        if (c == '-') {
+          break;
+        } else {
+          pos++;
+        }
+      }
+
+      if (pos <= reguaNova->getRegua().size()) {
+        if (pos - 2 >= 0) {
+          vector<char> aux = reguaNova->getRegua();
+          iter_swap(aux.begin() + pos - 2, aux.begin() + pos);
+          reguaNova->setRegua(aux);
+        }
+      } else {
+        reguaNova = NULL;
+      }
+      break;
+    } break;
+    case 4: {
+      // Bloco dois a Direita move para o buraco
+      int pos = 0;
+
+      for (char &c : reguaNova->getRegua()) {
+        if (c == '-') {
+          break;
+        } else {
+          pos++;
+        }
+      }
+
+      if (pos <= reguaNova->getRegua().size()) {
+        if (pos + 2 <= reguaNova->getRegua().size()) {
+          vector<char> aux = reguaNova->getRegua();
+          iter_swap(aux.begin() + pos + 2, aux.begin() + pos);
+          reguaNova->setRegua(aux);
+        }
+
+      } else {
+        reguaNova = NULL;
+      }
+      break;
+    }
+    default:
+
+      break;
+    }
+
+    return reguaNova;
+  };
+
 private:
   // Funções
 
-  void calculaEstadoFinal();
-  bool validade();
-  Estado *movimentar(int indexMovimento);
+  void calculaEstadoFinal() {
+    if (n % 2 != 0) {
+      for (int j = 0; j <= (n / 2) - 1; j++) {
+        this->estadoFinal[j] = 'B';
+      }
+      this->estadoFinal[n / 2] = '-';
+      for (int j = (n / 2) + 1; j < (n); j++) {
+        this->estadoFinal[j] = 'P';
+      }
+    } else {
+      for (int j = 0; j <= (n % 2) - 1; j++) {
+        this->estadoFinal[j] = 'B';
+      }
+      this->estadoFinal[n % 2] = '-';
+      for (int j = (n / 2) % 1; j < (n); j++) {
+        this->estadoFinal[j] = 'P';
+      }
+    }
+  };
 
   // Variaveis
   int n;
   int numMov;
   vector<char> regua;
   vector<char> estadoFinal;
-
-public:
-  ReguaPuzzle();
-  ReguaPuzzle(int n, vector<char> regua);
-  ~ReguaPuzzle();
-
-  // Setters
-  void setN(int novoN);
-  void setRegua(vector<char> regua);
-
-  // getters
-  int getN();
-  vector<char> getRegua();
-  int getNumMovimentos();
-  vector<char> getEstadoFinal();
-
-  // Funções de heuristica: 
-
-  Estado *movimentar(int indexMovimento, bool custo, Estado* estadoFinal);
-
-  bool equals(Estado *estado);
-  // Imprimir estado atual
-  void print();
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include <string>
 #include <vector>
 
+#include "Algoritmos/Informados/AEstrela.cpp"
+#include "Algoritmos/Informados/BuscaGulosa.cpp"
+#include "Algoritmos/Informados/IDAEstrela.cpp"
 #include "Algoritmos/NaoInformados/Backtracking.cpp"
 #include "Algoritmos/NaoInformados/BuscaEmLargura.cpp"
 #include "Algoritmos/NaoInformados/BuscaEmProfundidade.cpp"
 #include "Algoritmos/NaoInformados/BuscaOrdenada.cpp"
-#include "Algoritmos/Informados/BuscaGulosa.cpp"
-#include "Algoritmos/Informados/AEstrela.cpp"
-#include "Algoritmos/Informados/IDAEstrela.cpp"
 #include "Estruturas/Headers/Estado.hpp"
 #include "Estruturas/Jarros.cpp"
 #include "Estruturas/ReguaPuzzle.hpp"
@@ -37,44 +37,35 @@ using namespace std;
  * Desde: 13/07/2022
  *******************************************/
 
-int main(int argc, char *argv[])
-{
-  if (argc == 1)
-  {
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
     runSemArgumentos();
     return 0;
-  }
-  else
-  {
+  } else {
 
-    if (argc == 3)
-    {
+    if (argc == 3) {
       inputFile.open(argv[1], ios::in);   // Abre o Arquivo de Entrada
       outputFile.open(argv[2], ios::out); // Abre o Arquivo de Saida
 
-      if (!inputFile)
-      {
+      if (!inputFile) {
         cerr << "Erro ao Abrir o Arquivo de Entrada!" << endl;
         exit(-1);
       }
 
-      if (!outputFile)
-      {
+      if (!outputFile) {
         cerr << "Erro ao Abrir o Arquivo de Saida! " << endl;
         exit(-1);
       }
-    }
-    else
-    {
+    } else {
       cerr << "O Algoritmo não localizou os arquivos de Entrada ou de Saída!"
            << endl;
       cerr << "Verifique o Formato de entrada <nome_do_executavel> "
-              "<arquivo_de_entrada> <arquivo_de_saida>"
-           << "Por exemplo: ab ba";
+              "<arquivo_de_entrada> <arquivo_de_saida>";
+
       cerr << "O arquivo de entrada deve ser formatado da seguinte forma: \n"
            << "Um regua com uma das peças representadas por B e a outra por P, "
               "com um espaço representado por '-' "
-           << endl;
+           << "Por exemplo: PB-BP" << endl;
       exit(-1);
     }
 
@@ -93,64 +84,76 @@ int main(int argc, char *argv[])
     int opcaoEscolhida2;
     string arquivo = argv[1];
     vector<char> regua;
-    ReguaPuzzle *reg;
 
-    int count = 1;
-    char letter; // variable for input value
-
+    int count = 0;
+    char letter;
     inputFile >> letter;
     cout << "Regua: ";
-    while (letter != ';')
-    {
+    outputFile<<"Regua: ";
+    while (letter != ';') {
       cout << letter;
+      outputFile<<letter;
       regua.push_back(letter);
       inputFile >> letter;
       count++;
     }
 
     cout << endl;
+    outputFile<<endl;
 
     n = count;
+    cout<<"Com tamanho: "<<n<<'\n';
+    ReguaPuzzle *reg = new ReguaPuzzle(n, regua);
 
-    while (menu1)
-    {
+/*
+    cout<<"DEBUG:"<<endl;
+    reg->print();
+*/
 
-      menu();
-      cin >> opcaoEscolhida;
-      switch (opcaoEscolhida)
-      {
-      case 0:
+  if (reg->validade()) {
 
-        inputFile.close();
-        outputFile.close();
-        cout << "Fechando programa" << endl;
-        exit(0);
-        break;
-      case 1:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 2:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 3:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 4:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 5:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 6:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      case 7:
-        cout << "Ainda Não implementado" << endl;
-        break;
-      default:
-        cout << "Parametro invalido, favor escolher uma opção valida";
-        break;
+      while (menu1) {
+
+        menu();
+        cin >> opcaoEscolhida;
+        switch (opcaoEscolhida) {
+        case 0:
+
+          inputFile.close();
+          outputFile.close();
+          cout << "Fechando programa" << endl;
+          exit(0);
+          break;
+        case 1:
+         cout << "Ainda Não implementado" << endl;
+         //  Backtracking::run(reg, reg->getEstadoFinal());
+          break;
+        case 2:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        case 3:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        case 4:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        case 5:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        case 6:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        case 7:
+          cout << "Ainda Não implementado" << endl;
+          break;
+        default:
+          cout << "Parametro invalido, favor escolher uma opção valida";
+          break;
+        }
       }
+   } else {
+
+      cout << "A regua não é valida, não é possivel rodar o programa";
     }
   }
   return 0;
@@ -159,8 +162,7 @@ int main(int argc, char *argv[])
 /*
  * Função que roda o programa sem um arquivo para ler ou para salvar
  */
-void runSemArgumentos()
-{
+void runSemArgumentos() {
 
   cout << "\t\t\tTrabalho de Inteligência Artificial" << endl;
   cout << "Alunos: Luan Reis Ciribelli e João Pedro Carvalho Lima" << endl;
@@ -170,29 +172,74 @@ void runSemArgumentos()
   int opcaoEscolhida;
   int opcaoEscolhida2;
   bool jarros = false;
-
-  while (true)
-  {
+  vector<char> regua;
+  while (true) {
     cout << "\nFavor escolhe o tamanho N da regua:";
     cin >> n;
 
-    if (n > 0)
-    {
+    if (n > 0) {
       break;
-    }
-    else
-    {
+    } else {
       cout << "Favor escolher um numero valido!";
     }
   }
 
-  while (menu1)
-  {
+  bool espaco = false;
+  cout << "\nPara montar a régua favor escolher P para peças pretas B para "
+          "peças brancas e \'-\' para o espaço:";
+  char aux = ' ';
+  while (regua.size() != n) {
+
+    if (regua.size() == n - 1 && !espaco) {
+      regua.push_back('-');
+      break;
+    }
+
+    cin >> aux;
+    switch (aux) {
+    case 'p':
+      regua.push_back(toupper(aux));
+      break;
+    case 'P':
+      regua.push_back(aux);
+      break;
+    case 'b':
+      regua.push_back(aux);
+      break;
+    case 'B':
+      regua.push_back(toupper(aux));
+      break;
+    case '-':
+      if (!espaco) {
+        regua.push_back(toupper(aux));
+        espaco = true;
+
+      } else {
+        cout
+            << "Só é possivel ter um buraco no jogo, favor selecionar uma peça";
+      }
+      break;
+    default:
+
+      cout << "Essa peça não é valida! Escolha outra";
+      break;
+    }
+  }
+
+  ReguaPuzzle *reg = new ReguaPuzzle(n, regua);
+
+  cout << "A régua final é:";
+
+  for (char &c : regua) {
+    cout << c;
+    cout << " ";
+  }
+if (reg->validade()) {
+  while (menu1) {
     menu();
     cin >> opcaoEscolhida;
 
-    switch (opcaoEscolhida)
-    {
+    switch (opcaoEscolhida) {
     case 0:
       cout << "Fechando programa" << endl;
       exit(0);
@@ -226,15 +273,17 @@ void runSemArgumentos()
       cout << "Parametro invalido, favor escolher uma opção valida";
       break;
     }
+  }}else{
+      cout<<"A régua é invalida, entrando no modo jarro - Apenas para os desenvolvedores";
+      menu1 = false;
+      jarros = true;
   }
   limpaTela();
-  while (jarros)
-  {
+  while (jarros) {
 
     menu2();
     cin >> opcaoEscolhida2;
-    switch (opcaoEscolhida2)
-    {
+    switch (opcaoEscolhida2) {
     case 0:
       cout << "Fechando programa" << endl;
       exit(0);
@@ -256,7 +305,7 @@ void runSemArgumentos()
       break;
     case 4:
       cout << "Busca Ordenada" << endl;
-      // BuscaOrdenada::run(new Jarros(0, 0, 0), new Jarros(0, 0, 0));
+       BuscaOrdenada::run(new Jarros(0, 0, 0), new Jarros(0, 0, 0));
       break;
     case 5:
       cout << "Ainda Não implementado" << endl;
@@ -278,8 +327,7 @@ void runSemArgumentos()
 /*
  * Menu para interação com usuario da ferramenta
  */
-void menu()
-{
+void menu() {
   cout << "\t\tAlgoritmos para o regua-puzzle:" << endl;
   cout << "[01] - Backtracking; " << endl;
   cout << "[02] - Busca em Largura; " << endl;
@@ -296,8 +344,7 @@ void menu()
  * Menu iterativo criado para os desenvolvedores, testar os algoritmos num caso
  * mais simples
  */
-void menu2()
-{
+void menu2() {
   cout << "\t\tAlgoritmos para o Jogo dos Jarros" << endl;
   cout << "[01] - Backtracking; " << endl;
   cout << "[02] - Busca em Largura; " << endl;
@@ -313,10 +360,8 @@ void menu2()
 /*
  * Função que limpa o terminal
  */
-void limpaTela()
-{
-  for (int i = 0; i < 50; i++)
-  {
+void limpaTela() {
+  for (int i = 0; i < 50; i++) {
     cout << "\n";
   }
 }
